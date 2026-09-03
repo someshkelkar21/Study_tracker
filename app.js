e2a949444ddbd4e9f87acce714911de802120b36
Somesh <someshkelkar21@gmail.com>
2026-09-03 21:55:51 +0530

    feat: add multiple subjects and progress calculation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let subject = {
+let subjects = [
+    {
     id: 1,
     name: "maths",
     chapters: [
@@ -24,7 +25,7 @@ let subject = {
             ]
         },
         {
-             id: 1,
+             id: 2,
             name: "Triangles",
             topics: [
                 {
@@ -46,9 +47,108 @@ let subject = {
     ]
 }
     ]
-}
-let completedCount = 0
+    },
+    {
+        id: 2,
+        name: "science",
+        chapters: [
+            {
+                id:1,
+                name: "matter in our surroundings",
+                topics: [
+                    {
+                        id:1,
+                        name: "states of matter",
+                        completed: true,
+                    },
+                    {
+                        id:2,
+                        name: "change of state",
+                        completed: true,
+                    },
+                    {
+                        id:3,
+                        name: "evaporation",
+                        completed: false,
+                    },
+                ]
+            },
+            {
+                id:2,
+                name: "atoms and molecules",
+                topics: [
+                    {
+                        id:1,
+                        name: "laws of chemical combination",
+                        completed: true,
+                    },
+                    {
+                        id:2,
+                        name: "atomic mass",
+                        completed: false,
+                    },
+                    {
+                        id:3,
+                        name: "molecules and chemical formulae",
+                        completed: false,
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "sst",
+        chapters: [
+            {
+                id:1,
+                name: "the french revolution",
+                topics: [
+                    {
+                        id:1,
+                        name: "causes of the revolution",
+                        completed: false,
+                    },
+                    {
+                        id:2,
+                        name: "french society",
+                        completed: false,
+                    },
+                    {
+                        id:3,
+                        name: "impact of the revolution",
+                        completed: true,
+                    },
+                ]
+            },
+            {
+                id:2,
+                name: "socialism in europe",
+                topics: [
+                    {
+                        id:1,
+                        name: "rise of socialist ideas",
+                        completed: false,
+                    },
+                    {
+                        id:2,
+                        name: "russian revolution",
+                        completed: false,
+                    },
+                    {
+                        id:3,
+                        name: "impact of socialism",
+                        completed: true,
+                    },
+                ]
+            }
+        ]
+    },
+]
+function getProgress(subject) {
+    let completedCount = 0
 let totalTopics =0
+
 for (let i = 0; i<subject.chapters.length; i++) {
 for (let j = 0; j <subject.chapters[i].topics.length; j++ ) {
 totalTopics++;
@@ -58,4 +158,11 @@ completedCount++;
 }
 }
 let progress = (completedCount/totalTopics)*100;
-console.log(progress);
+return progress;
+}
+let mathsProgress = getProgress(subjects[0]);
+console.log(mathsProgress);
+let scienceProgress = getProgress(subjects[1]);
+console.log(scienceProgress);
+let sstProgress = getProgress(subjects[2]);
+console.log(sstProgress);
